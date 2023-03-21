f95e109427dab30e71d5f1acf47b4fb22a836b2b
Joyce Njoroge <joycewambuku33@gmail.com>
2023-03-21 15:01:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer = "not bob";
+}
+function overWriteBestCustomer(){
+    window.bestCustomer = "not bob";
+}
+const leastFavoriteCustomer = "Kim"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob"
+}
